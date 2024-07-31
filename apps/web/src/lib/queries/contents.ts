5b22153e568d3029a51bb77f4895bd85e9616ff9
Sajarin M <sajarinm@yahoo.com>
2024-07-31 15:43:03 +0530

    remove logging

--- apps/web/src/lib/queries/contents.ts
+++ apps/web/src/lib/queries/contents.ts
@@ -5,7 +5,5 @@ export async function getHomePageContents() {
   const res = await fetch(apiUrl + '/api/contents');
   const data = await res.json();
 
-  console.log(res);
-
   return data as HomePageContentVm;
 }
